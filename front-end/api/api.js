21a68e9a6090c16fdcce73ee55bad9f3c3f1a341
leandro-ikehara <leandro.ikehara@gmail.com>
2025-05-12 16:30:33 -0300

    ajustes de configuração

--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
 
-// eslint-disable-next-line no-undef
-const NODE_ENV = process.env;
-const URL = NODE_ENV === 'development' ? 'http://localhost:3001/api': '/api';
-// const URL = 'http://localhost:3001';
+const URL = 'https://clone-spotify-ftf8.onrender.com/api';
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
