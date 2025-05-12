57bbddc1bbede89d3d104670bfe30a70d154e0b6
leandro-ikehara <leandro.ikehara@gmail.com>
2025-05-12 16:13:29 -0300

    ajustes de configuração

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from "cors";
 import { db } from "./connect.js";
+import path from 'path';
+
+const __dirname = path.resolve();
 
 
 const app = express();
@@ -8,17 +11,23 @@ const PORT = 3001;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/api/', (req, res) => {
+    res.send('Não tem nada por aqui!');
 });
 
-app.get('/artists', async (req, res) => {
+app.get('/api/artists', async (req, res) => {
     res.send(await db.collection('artists').find({}).toArray());
 });
 
-app.get('/songs', async (req, res) => {
+app.get('/api/songs', async (req, res) => {
     res.send(await db.collection('songs').find({}).toArray());
 });
+
+// app.use(express.static(path.join(__dirname, '../../front-end/dist')));
+
+// app.get('*', async (req, res) => {
+//     res.sendFile(path.join(__dirname, '../../front-end/dist/index.html'));
+// });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
--- front-end/api/api.js
+++ front-end/api/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3001';
+
+// eslint-disable-next-line no-undef
+const NODE_ENV = process.env;
+const URL = NODE_ENV === 'development' ? 'http://localhost:3001/api': '/api';
+// const URL = 'http://localhost:3001';
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
